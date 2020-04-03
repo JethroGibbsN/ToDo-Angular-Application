cbf537e63ec5cb42f00b1c3cf2b2da472486544b
Krishna Chandra Boyapati <41461972+JethroGibbsN@users.noreply.github.com>
2020-04-03 19:59:47 +0530

    multer

--- app/models/tasks.model.js
+++ app/models/tasks.model.js
@@ -2,17 +2,29 @@ module.exports = (sequelize, Sequelize) => {
 	const Task = sequelize.define('task', {
 	  taskName: {
         type: Sequelize.STRING,
-        primaryKey: true
+		primaryKey: true,
+		allowNull: false
 	  },
 	  taskDesc: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false
 	  },
 	  imgName: {
 		  type: Sequelize.STRING
 	  },
 	  imgData:{
 		  type: Sequelize.BLOB('long')
-	  }
+	  },
+	  created_at: {
+		type: Sequelize.DATE,
+		defaultValue: Sequelize.NOW,
+		allowNull: false
+	  },
+	  updated_at: {
+		type: Sequelize.DATE,
+		defaultValue: Sequelize.NOW,
+		allowNull: false
+	  },
 	});
 	
 	return Task;
