d6233ec9e6d55bbadd3788e4c1f90c97b01ba003
JethroGibbsN <boyapatikrishna99@gmail.com>
2020-04-03 11:59:15 +0530

    contoller

---
+++ app/routes/tasks.route.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+ 
+    const tasks = require('../controllers/tasks.controller.js');
+ 
+    // Create a new task
+    app.post('/tasks', tasks.create);
+ 
+    // Retrieve all task
+    app.get('/tasks', tasks.findAll);
+ 
+    // Retrieve a single task by name
+    app.get('/tasks/:taskName', tasks.findByName);
+ 
+    // Update a task with name
+    app.put('/tasks/:taskname', tasks.update);
+ 
+    // Delete a task with name
+    app.delete('/tasks/:taskname', tasks.delete);
+}
