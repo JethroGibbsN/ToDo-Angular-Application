ad8cabd7e3a3290f7d7c033ec4c1b6af246e2d9c
Krishna Chandra Boyapati <41461972+JethroGibbsN@users.noreply.github.com>
2021-02-16 14:32:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ const db = require('./app/db/db.js');
 db.sequelize.sync({ force: false , alter : true }).then(() => {
     console.log('Drop and Resync with { force: false }');
   });
-// app.get("/index", (req, res) => {
-//     res.json({status: "working", message: "Welcome"})
-// })
 require('./app/routes/tasks.route.js')(app);
-
 
 app.listen(5000,()=>console.log('Server @ port 5000'));
